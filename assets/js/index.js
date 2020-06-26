be5c5c51eb34a01902172342834bbe4f360d385c
Matheus Pantoja <matheus.pantoja@ingressorapido.com.br>
2020-06-26 10:57:27 -0300

    Remove bottão para o chat

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,6 @@ const openButton = document.getElementById('chatButton')
 const closeButton = document.getElementById('closeChat')
 
 
-openButton.onclick = function () {
-    chat.setAttribute('open', '')
-    openButton.style.display = 'none'
-    document.querySelector('main').style.overflow = 'hidden'
-}
-
 closeButton.onclick = function () {
     chat.removeAttribute('open')
     openButton.style.display = 'flex'
