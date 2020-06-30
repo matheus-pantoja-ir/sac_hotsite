2f028bb1f312a6a03a448bebc0a221c66b9e9c32
Matheus Pantoja <matheus.pantoja@ingressorapido.com.br>
2020-06-30 14:35:23 -0300

    Adiciona balão de informação e chat com sac

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,11 @@ const chat = document.getElementById('chat')
 const openButton = document.getElementById('chatButton')
 const closeButton = document.getElementById('closeChat')
 
+openButton.onclick = function () {
+    chat.setAttribute('open', '')
+    openButton.style.display = 'none'
+    document.querySelector('main').style.overflow = 'hidden'
+}
 
 closeButton.onclick = function () {
     chat.removeAttribute('open')
